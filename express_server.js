08cdaf7e428bf305d49cc414350e71577d05d4d6
shivanix <shvni.here@gmail.com>
2021-07-05 22:42:10 +0000

    added a POST route to receive the form submission

--- express_server.js
+++ express_server.js
@@ -4,6 +4,22 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+function generateRandomString() {
+  const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const amount = 6;
+  let output = "";
+  for (let i = 0; i < amount; i++) {
+    let newCharIndex = Math.floor(Math.random() * chars.length);
+    output += chars[newCharIndex];
+  }
+  return output;
+}
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -14,12 +30,25 @@ app.get("/", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    urls: urlDatabase
+  };
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+/*The GET /urls/new route needs to be defined before the GET /urls/:id route. Routes defined earlier will take precedence,
+ so if we place this route after the /urls/:id definition, any calls to /urls/new will be handled by app.get("/urls/:id", ...) 
+ because Express will think that new is a route parameter*/
+
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL};
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: req.params.longURL
+  };
   res.render("urls_show", templateVars);
 });
 
@@ -31,14 +60,10 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-/*app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
- });
- 
- app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
- });*/
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
